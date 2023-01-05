3a935caa3f0b0d582d11ba2b2f2f2cea2facb75a
Christopher Stevers <christopher.stevers@protonmail.com>
2023-01-05 11:58:39 -0500

    added more logs

--- src/email/getOffChainData.ts
+++ src/email/getOffChainData.ts
@@ -4,9 +4,9 @@ import GET_USER from '../graphql/getUser.js';
 const getOffChainData = async (github: string, uuid: string) => {
   const githubObj = github ? { github } : {};
   const uuidObj = uuid ? { uuid } : {};
-
   try {
     const url: string = process.env['OPENQ_API_URL']!;
+    console.log('offURl', url);
     const result: AxiosResponse = await axios.post(`${url}/graphql`, {
       query: GET_USER,
 
